9490213018e5d5b78ec3298dd62e0e97a1cbe3f4
Clément BAILLY-GRANDVAUX <clement.bailly-grandvaux@telecomnancy.eu>
2021-10-28 15:53:13 +0200

    Possibilité d'éxécuter des commandes

--- tesh.c
+++ tesh.c
@@ -6,11 +6,16 @@
 #include <string.h>
 #include <limits.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <limits.h>
 #include <pwd.h>
 
 #define BUFFER_LENGTH 4096
 #define ARG_MAX 512
+
+#define STDIN_FD 0
+#define STDOUT_FD 1
+#define STDERR_FD 2
 
 bool stop = false;
 
@@ -20,21 +25,76 @@ int decouper(char* entree, char **sortie) {
     char *next = strchr(base, ' ');
 
     while(next != NULL) {
-        next[0] = '\0';
         sortie[i++] = base;
-        char *temp = next + 1;
-        next = strchr(entree, ' ');
-        entree = temp;
+
+        // On enlève les espaces en trop
+        while (next[0] == ' ' || next[0] == '\t') {
+            next[0] = '\0'; // On remplace l'espace par un \0
+            next++;
+        }
+
+        // Si il y avait des espaces inutiles à la fin de la commande
+        if (next[0] == '\n') {
+            next[0] = '\0';
+            sortie[i] = NULL;
+            return i;
+        }
+
+        base = next;
+        next = strchr(base, ' ');
     }
 
     if(entree[0] != '\0') {
         sortie[i++] = base;
-        strchr(entree, '\n')[0] = '\0';
+        strchr(base, '\n')[0] = '\0';
     }
+
+    sortie[i] = NULL;
 
     return i;
 }
 
+/*
+Lance un exécutable et gère la sortie et l'entrée
+ char* file : fichier à éxécuter
+ char *argv[] : paramètres
+ int input : file descriptor pour l'entrée (par défaut stdin)
+ bool stdout : si le retour doit être stdout ou un pipe créé
+
+ int de retour : file descritor de sortie (par défaut stdout)
+*/
+int run(const char* file, char *args[], int input, bool to_stdout) {
+    int fd[2];
+    if (!to_stdout)
+        pipe(fd);
+
+    if(!fork()) {
+        //si fils
+        if (input != STDIN_FD) {
+            dup2(input, STDIN_FD);
+        }
+        if (!to_stdout) {
+            dup2(fd[1], STDOUT_FD);
+        }
+
+        int status = execvp(file, args);
+        if (status == -1) {
+            fprintf(stderr, "Ne peut pas exécuter \"%s\" Code d'erreur : %d\n", file, status);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // Plus besoin d'écire dans le pipe, c'est au fils de le faire
+    close(fd[1]);
+
+    if (input != STDIN_FD)
+        close(input);
+
+    if (!to_stdout)
+        return fd[0];
+
+    return STDOUT_FD;
+}
 
 
 int main(int argc, char *argv[]) {
@@ -72,12 +132,7 @@ int main(int argc, char *argv[]) {
             printf("Erreur lors de la récupération du répertoire courant.\n");
             return EXIT_FAILURE;
         }
-        if (!next_silent) {
-            printf("%s@%s:%s$ ", username, hostname, path);
-        }
-        else {
-            next_silent = false;
-        }
+        printf("%s@%s:%s$ ", username, hostname, path);
         // Flush le buffer de stdout pour que le USER@HOSTNAME:REPCOURANT$ sans retour à la ligne
         fflush(stdout);
         if (fgets(input_buffer, BUFFER_LENGTH, stdin) == NULL) {
@@ -87,8 +142,7 @@ int main(int argc, char *argv[]) {
         memcpy(entree, input_buffer, BUFFER_LENGTH);
         int nbargs = decouper(entree, current_args);
 
-        if (nbargs == 0) {
-            next_silent = true;
+        if (nbargs == 0 || current_args[0][0] == '\0') {
             continue;
         }
 
@@ -103,6 +157,11 @@ int main(int argc, char *argv[]) {
             if (chdir(path) != 0) {
                 printf("Erreur lors du changement du répertoire courant à %s\n", path);
             }
+        }
+        else {
+            int status;
+            run(current_args[0], current_args, STDIN_FD, true);
+            waitpid(-1, &status, 0);
         }
     }
 
