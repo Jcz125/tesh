d34f0fa177e2565e8ac37627c7a3597fc6e722b3
Céline Zhang <celine.zhang@telecomnancy.eu>
2021-11-01 03:33:23 +0100

    ajout de la fonction create_fd et des cases de switch

--- tesh.c
+++ tesh.c
@@ -84,7 +84,7 @@ char** search(char** base, char** end, int *spe_i) {
 }
 
 /* Reordonne l'entree decoupé pour :
- avoir la redirection vers des ficheirs au début (que la dernière redirection donnée est gardée)
+ avoir la redirection vers des fichiers au début (que la dernière redirection donnée est gardée)
  avoir la redirection d'un fichier en entree au début (que la dernière redirection donnée est gardée)
  Exemples :
     ls > fichier1 -la > fichier2
@@ -145,6 +145,24 @@ void reorder(char** base, char** next) {
     }
 }
 
+/*
+Créer le file descriptor en cas d'existance d'un élément de redirection entre start et base
+ char** start : début de la séquence à traiter
+ char** base : position de la commande
+ int* last_out : sortie de la denière commande exécutée (en général un fd ou un stdin si aucune sortie)
+ 
+ exemple : > fichier ls | grep "d"
+ start pointe sur >
+ base pointe sur ls
+ last_out = 0 (ici)
+ create_fd voit un >, elle crée le fd et return le fd sans modifier last_out (car inutile)
+ dans le cas de <, elle crée le fd et return le fd en modifiant last_out 
+ (car on lit dans le fichier pour passer le contenu dans la commande pointée par base via last_out qui sera mis à jour)
+*/
+int create_fd(char** start, char** base, int* last_out) {
+    // à compléter (soon)
+    return NULL;
+}
 
 /*
 Lance un exécutable et gère la sortie et l'entrée
@@ -251,72 +269,101 @@ int main(int argc, char *argv[]) {
             char **next;
             char **end = entree_decoupee + nbargs;
             int status;
+            int last_out = STDIN_FD;
+            int old_out = NULL;
+            char **start = entree_decoupee;
+            int fd = NULL;
             while (base < end) {
                 int spe_i;
                 next = search(base, end, &spe_i);
-                //reorder(base, next);
+                reorder(base, next);    // base tombe sur la commande à exécuter
 
-                // on en lève le caractère spécial pour que base puisse être donné directement à execvp
+                // on enlève le caractère spécial pour que base puisse être donné directement à execvp
                 next[0] = NULL;
                 switch (spe_i) {
                     case 0: // ;
-                    //     run((entree_decoupee + base)[0], (entree_decoupee + base), last_out, true);
-                    //     last_out = STDIN_FD;
-                    //     waitpid(-1, &status, 0);
-                    //     break;
+                        old_out = last_out;
+                        fd = create_fd(start, base, &last_out);         // à écrire : créer le file director s'il y a un élément de redirection entre debut et base (qui pointe sur la première commande à exécuter), sinon renvoi NULL
+                        run((base)[0], (base), last_out, true);
+                        if (old_out == last_out && fd)                  // ça veut dire qu'on est dans le cas de > ou >>
+                            dup2(fd, last_out);
+                        last_out = STDIN_FD;
+                        waitpid(-1, &status, 0);
+                        break;
+/*
+                    case 1: // >
+                        last_out = run((base)[0], (base), last_out, false);
+                        waitpid(-1, &status, 0);
+                        int fd = fopen("%c", "w", (next + 1)[0]);     // ouvre ou créer le fichier (en écriture) avec la chaîne se trouvant après >
+                        dup2(last_out, fd);     // peut-on utiliser write ?
+                        close(fd);
+                        base = ++next;
+                        break;
 
-                    // case 1: // >
-                    //     last_out = run((entree_decoupee + base)[0], (entree_decoupee + base), last_out, false);
-                    //     waitpid(-1, &status, 0);
-                    //     int fd = fopen("%c", "w", (entree_decoupee + next + 1)[0]);     // ouvre ou créer le fichier (en écriture) avec la chaîne se trouvant après >
-                    //     dup2(last_out, fd);     // peut-on utiliser write ?
-                    //     close(fd);
-                    //     base = ++next;
-                    //     break;
+                    case 2: // >>
+                        last_out = run((base)[0], (base), last_out, false);
+                        waitpid(-1, &status, 0);
+                        int fd = fopen("%c", "a", (next + 1)[0]);     // ouvre ou créer le fichier (en ajout) avec la chaîne se trouvant après >
+                        dup2(last_out, fd);
+                        close(fd);
+                        base = ++next;
+                        break;
+*/
+                    case 1: // |
+                        old_out = last_out;
+                        fd = create_fd(debut, base, &last_out);
+                        if (old_out == last_out & fd) {
+                            last_out = run((base)[0], (base), last_out, false);
+                            dup2(fd, last_out);
+                            last_out = STDIN_FD;
+                        }
+                        else
+                            last_out = run((base)[0], (base), last_out, false);
+                        waitpid(-1, &status, 0);        // dois-je décaler ça pour mettre juste après les run ?
+                        break;
 
-                    // case 2: // >>
-                    //     last_out = run((entree_decoupee + base)[0], (entree_decoupee + base), last_out, false);
-                    //     waitpid(-1, &status, 0);
-                    //     int fd = fopen("%c", "a", (entree_decoupee + next + 1)[0]);     // ouvre ou créer le fichier (en ajout) avec la chaîne se trouvant après >
-                    //     dup2(last_out, fd);
-                    //     close(fd);
-                    //     base = ++next;
-                    //     break;
+                    case 2: // &&
+                        old_out = last_out;
+                        fd = create_fd(debut, base, &last_out);
+                        run((base)[0], (base), last_out, true);
+                        if (old_out == last_out && fd)
+                            dup2(fd, last_out);
+                        last_out = STDIN_FD;
+                        waitpid(-1, &status, 0);
+                        if (status != 0)        // si la commande avant && ne s'est pas exécutée correctement
+                            base = end;         // on ignore la commande après && donc ici on quitte la boucle while(base < end)
+                            printf("une commande ne s'est pas lancé correctement\n");
+                        break;
 
-                    // case 3: // |
-                    //     last_out = run((entree_decoupee + base)[0], (entree_decoupee + base), last_out, false);
-                    //     waitpid(-1, &status, 0);
-                    //     break;
-
-                    // case 4: // &&
-                    //     run((entree_decoupee + base)[0], (entree_decoupee + base), last_out, true);
-                    //     last_out = STDIN_FD;
-                    //     waitpid(-1, &status, 0);
-                    //     if (status != 0)        // si la commande avant && ne s'est pas exécutée correctement
-                    //         base = nbargs;      // on ignore la commande après && donc ici on quitte la boucle while(base < nbargs)
-                    //         printf("une commande ne s'est pas lancé correctement\n");
-                    //     break;
-
-                    // case 5: // ||
-                    //     run((entree_decoupee + base)[0], (entree_decoupee + base), last_out, true);
-                    //     last_out = STDIN_FD;
-                    //     waitpid(-1, &status, 0);
-                    //     if (status == 0)        // si la commande avant || s'est exécutée sans erreur
-                    //         base = nbargs;      // on ignore la commande après || donc ici on quitte la boucle while(base < nbargs)
-                    //     break;
-
-                    // case 6: // <
-                    //     base = ++next;
-                    //     int fd = fopen("%c", "r", (entree_decoupee + base)[0]);     // ouvre le fichier (en lecture)
-                    //     last_out = run((entree_decoupee + base - 2)[0], (entree_decoupee + base - 2), fd, false);
-                    //     break;
-
+                    case 3: // ||
+                        old_out = last_out;
+                        fd = create_fd(debut, base, &last_out);
+                        run((base)[0], (base), last_out, true);
+                        if (old_out == last_out && fd)
+                            dup2(fd, last_out);
+                        last_out = STDIN_FD;
+                        waitpid(-1, &status, 0);
+                        if (status == 0)        // si la commande avant || s'est exécutée sans erreur
+                            base = end;         // on ignore la commande après || donc ici on quitte la boucle while(base < end)
+                        break;
+/*
+                    case 6: // <
+                        base = ++next;
+                        int fd = fopen("%c", "r", (base)[0]);     // ouvre le fichier (en lecture)
+                        last_out = run((base - 2)[0], (base - 2), fd, false);
+                        break;
+*/
                     default:
-                        run(base[0], base, STDIN_FD, true);
+                        old_out = last_out;
+                        fd = create_fd(debut, base, &last_out);
+                        run(base[0], base, last_out, true);
+                        if (old_out == last_out && fd)
+                            dup2(fd, last_out);
                         waitpid(-1, &status, 0);
                         break;
                 }
                 base = ++next;
+                start = base;
             }
         }
     }
